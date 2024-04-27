da9c33cc8551b7f99a01b6dbb6a4ba1dcc0ffc3e
unknown <siraj115@gmail.com>
2024-04-27 21:51:49 +0530

    Update variable

--- routes/Users.js
+++ routes/Users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 
-var UserModule = require("../modules/UserModule");
+const UserModule = require("../modules/UserModule");
 
 
 router.get("/listuser", UserModule.listUser);  
